a6cfd514a9f77eef22de5f3f77b8c9b9860f58d8
Sherif Awad <eng.sherifawad@gmail.com>
2022-06-08 15:21:47 +0200

    add selection

--- components/UsersList.tsx
+++ components/UsersList.tsx
@@ -1,9 +1,20 @@
+import { useRef, useState } from "react";
+import AddStudentModel from "./AddStudentModel";
 import UsersListItem from "./UsersListItem";
 
 function UsersList() {
+	const [isOpened, setIsOpened] = useState(false);
+
+	const onProceed = () => {
+		console.log("Proceed clicked");
+	};
 	return (
 		<div className="grid grid-row-[auto_1fr] gap-8">
-			<button className="self-end shadow-sm px-8 py-1 rounded-full bg-green-300 text-white">
+			<button
+				onClick={() => setIsOpened(true)}
+				className="justify-self-end block w-32 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+				type="button"
+			>
 				Add Student
 			</button>
 			<UsersListItem
@@ -14,6 +25,8 @@ function UsersList() {
 				grade="1st Grade"
 				group="SMW"
 			/>
+
+			<AddStudentModel isOpened={isOpened} onProceed={onProceed} onClose={() => setIsOpened(false)} />
 		</div>
 	);
 }
