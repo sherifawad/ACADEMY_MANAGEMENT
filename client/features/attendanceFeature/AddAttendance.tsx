a4216b438af0d1df11a766f985a7d3b74b06a6f7
Sherif Awad <eng.sherifawad@gmail.com>
2022-07-29 21:05:44 +0200

    exam condition UI

--- client/features/attendanceFeature/AddAttendance.tsx
+++ client/features/attendanceFeature/AddAttendance.tsx
@@ -103,233 +103,136 @@ function AddAttendance({
 
 	return (
 		<form method="dialog" className="space-y-6" action="#" ref={mainRef}>
-			<div className="flex flex-col gap-4">
-				<div className="accordion-item">
-					<h2 id="accordion-flush-heading-3">
-						<button
-							type="button"
-							className="flex items-center justify-between w-full py-5 font-medium text-left text-white  dark:text-gray-400"
-							data-accordion-target="#accordion-flush-body-3"
-							aria-expanded="false"
-							aria-controls="accordion-flush-body-3"
-							onClick={handleExpandCollapseClick}
-						>
-							<span>Edit Multiple Student</span>
-							<svg
-								style={{ transform: value ? "rotate(180deg)" : "" }}
-								data-accordion-icon
-								className="w-6 h-6 shrink-0 transform rotate-180"
-								fill="currentColor"
-								viewBox="0 0 20 20"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									fill-rule="evenodd"
-									d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-									clip-rule="evenodd"
-								></path>
-							</svg>
-						</button>
-					</h2>
-					<div
-						id="accordion-flush-body-3"
-						className={`${value ? "" : "hidden"}`}
-						aria-labelledby="accordion-flush-heading-3"
+			<div className="accordion-item">
+				<h2 id="accordion-flush-heading-3">
+					<button
+						type="button"
+						className="flex items-center justify-between w-full py-5 font-medium text-left text-white  dark:text-gray-400"
+						data-accordion-target="#accordion-flush-body-3"
+						aria-expanded="false"
+						aria-controls="accordion-flush-body-3"
+						onClick={handleExpandCollapseClick}
 					>
-						<div className="py-5 font-light">
-							<div className="flex flex-col gap-4">
-								<DateTimePicker
-									locale={arEG}
-									datePlaceholder="Date"
-									timePlaceholder="Time"
-									timePrecision={15}
-									timeFormat="hh:mm a"
-									date={startAtCondition}
-									onChange={setStartAtCondition}
-								>
-									{({ dateInputProps, timeInputProps }) => (
-										<div className="w-full">
-											<label
-												htmlFor="start"
-												className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-											>
-												change attendance where start is:
-											</label>
-											<div className="flex gap-4">
-												<input
-													{...dateInputProps}
-													name="startDate"
-													id="startDate"
-													className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-													required
-												/>
-												<input
-													{...timeInputProps}
-													name="startTime"
-													id="startTime"
-													className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-													required
-												/>
-											</div>
+						<span>Edit Multiple Student</span>
+						<svg
+							style={{ transform: value ? "rotate(180deg)" : "" }}
+							data-accordion-icon
+							className="w-6 h-6 shrink-0 transform rotate-180"
+							fill="currentColor"
+							viewBox="0 0 20 20"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+								clip-rule="evenodd"
+							></path>
+						</svg>
+					</button>
+				</h2>
+				<div
+					id="accordion-flush-body-3"
+					className={`${value ? "" : "hidden"}`}
+					aria-labelledby="accordion-flush-heading-3"
+				>
+					<div className="py-5 font-light">
+						<div className="flex flex-col gap-4">
+							<DateTimePicker
+								locale={arEG}
+								datePlaceholder="Old Start Date"
+								timePlaceholder="Old Start Time"
+								timePrecision={15}
+								timeFormat="hh:mm a"
+								date={startAtCondition}
+								onChange={setStartAtCondition}
+							>
+								{({ dateInputProps, timeInputProps }) => (
+									<div className="w-full">
+										<label
+											htmlFor="start"
+											className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+										>
+											change attendance where start is:
+										</label>
+										<div className="flex gap-4">
+											<input
+												{...dateInputProps}
+												name="startDate"
+												id="startDate"
+												className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+												required
+											/>
+											<input
+												{...timeInputProps}
+												name="startTime"
+												id="startTime"
+												className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+												required
+											/>
 										</div>
-									)}
-								</DateTimePicker>
+									</div>
+								)}
+							</DateTimePicker>
 
-								<DateTimePicker
-									locale={arEG}
-									datePlaceholder="Date"
-									timePlaceholder="Time"
-									timePrecision={15}
-									timeFormat="hh:mm a"
-									date={endAtCondition}
-									onChange={setEndAtCondition}
-								>
-									{({ dateInputProps, timeInputProps }) => (
-										<div className="w-full">
-											<label
-												htmlFor="start"
-												className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-											>
-												change attendance where end is:
-											</label>
-											<div className="flex gap-4">
-												<input
-													{...dateInputProps}
-													name="startDate"
-													id="startDate"
-													className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-													required
-												/>
-												<input
-													{...timeInputProps}
-													name="startTime"
-													id="startTime"
-													className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-													required
-												/>
-											</div>
+							<DateTimePicker
+								locale={arEG}
+								datePlaceholder="Old End Date"
+								timePlaceholder="Old End Time"
+								timePrecision={15}
+								timeFormat="hh:mm a"
+								date={endAtCondition}
+								onChange={setEndAtCondition}
+							>
+								{({ dateInputProps, timeInputProps }) => (
+									<div className="w-full">
+										<label
+											htmlFor="start"
+											className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+										>
+											change attendance where end is:
+										</label>
+										<div className="flex gap-4">
+											<input
+												{...dateInputProps}
+												name="startDate"
+												id="startDate"
+												className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+												required
+											/>
+											<input
+												{...timeInputProps}
+												name="startTime"
+												id="startTime"
+												className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+												required
+											/>
 										</div>
-									)}
-								</DateTimePicker>
+									</div>
+								)}
+							</DateTimePicker>
 
-								<div className="row-span-full">
-									<label
-										htmlFor="note"
-										className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-									>
-										change attendance where Note contains:
-									</label>
-									<input
-										type="text"
-										name="note"
-										id="note"
-										value={noteCondition}
-										onChange={(e) => setNoteCondition(e.target.value)}
-										className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-										placeholder="Note"
-									/>
-								</div>
+							<div className="row-span-full">
+								<label
+									htmlFor="note"
+									className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+								>
+									change attendance where Note contains:
+								</label>
+								<input
+									type="text"
+									name="note"
+									id="note"
+									value={noteCondition}
+									onChange={(e) => setNoteCondition(e.target.value)}
+									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+									placeholder="Old Note"
+								/>
 							</div>
 						</div>
 					</div>
 				</div>
-
-				{edit && (
-					<div className="flex flex-col gap-4">
-						<DateTimePicker
-							locale={arEG}
-							datePlaceholder="Date"
-							timePlaceholder="Time"
-							timePrecision={15}
-							timeFormat="hh:mm a"
-							date={startAtCondition}
-							onChange={setStartAtCondition}
-						>
-							{({ dateInputProps, timeInputProps }) => (
-								<div className="w-full">
-									<label
-										htmlFor="start"
-										className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-									>
-										change attendance where start is:
-									</label>
-									<div className="flex gap-4">
-										<input
-											{...dateInputProps}
-											name="startDate"
-											id="startDate"
-											className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-											required
-										/>
-										<input
-											{...timeInputProps}
-											name="startTime"
-											id="startTime"
-											className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-											required
-										/>
-									</div>
-								</div>
-							)}
-						</DateTimePicker>
-
-						<DateTimePicker
-							locale={arEG}
-							datePlaceholder="Date"
-							timePlaceholder="Time"
-							timePrecision={15}
-							timeFormat="hh:mm a"
-							date={endAtCondition}
-							onChange={setEndAtCondition}
-						>
-							{({ dateInputProps, timeInputProps }) => (
-								<div className="w-full">
-									<label
-										htmlFor="start"
-										className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-									>
-										change attendance where end is:
-									</label>
-									<div className="flex gap-4">
-										<input
-											{...dateInputProps}
-											name="startDate"
-											id="startDate"
-											className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-											required
-										/>
-										<input
-											{...timeInputProps}
-											name="startTime"
-											id="startTime"
-											className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-											required
-										/>
-									</div>
-								</div>
-							)}
-						</DateTimePicker>
-
-						<div className="row-span-full">
-							<label
-								htmlFor="note"
-								className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-							>
-								change attendance where Note contains:
-							</label>
-							<input
-								type="text"
-								name="note"
-								id="note"
-								value={noteCondition}
-								onChange={(e) => setNoteCondition(e.target.value)}
-								className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-								placeholder="Note"
-							/>
-						</div>
-					</div>
-				)}
-
+			</div>
+			<div className="flex flex-col gap-4">
 				<DateTimePicker
 					locale={arEG}
 					datePlaceholder="Date"
--- client/features/examFeature/AddExam.tsx
+++ client/features/examFeature/AddExam.tsx
@@ -3,7 +3,7 @@ import {
 	createMultipleExamMutation,
 	CREATE_EXAM_MUTATION,
 	updateExamMutation,
-    updateMultipleExamMutation,
+	updateMultipleExamMutation,
 } from "features/examFeature/examMutations";
 import { createAxiosService } from "core/utils";
 import { format, parse } from "date-fns";
@@ -12,8 +12,11 @@ import { useEffect, useState } from "react";
 import { DatePicker } from "react-next-dates";
 import { useMutation } from "react-query";
 import { examInitialProperties } from "./examTypes";
+import useToggle from "customHooks.tsx/useToggle";
 
 function AddExam({ onProceed, onClose, initialExam, profileIds, edit }: examInitialProperties) {
+	const [value, toggleValue] = useToggle(edit || false);
+
 	const { profileId, score, date, note, id } = initialExam || {};
 
 	const [dateCondition, setDateCondition] = useState(null);
@@ -93,14 +96,126 @@ function AddExam({ onProceed, onClose, initialExam, profileIds, edit }: examInit
 		onClose();
 	};
 
+	const handleExpandCollapseClick = () => {
+		toggleValue(!value);
+	};
+
 	return (
 		<form method="dialog" className="space-y-6" action="#">
+			<div className="accordion-item">
+				<h2 id="accordion-flush-heading-3">
+					<button
+						type="button"
+						className="flex items-center justify-between w-full py-5 font-medium text-left text-white  dark:text-gray-400"
+						data-accordion-target="#accordion-flush-body-3"
+						aria-expanded="false"
+						aria-controls="accordion-flush-body-3"
+						onClick={handleExpandCollapseClick}
+					>
+						<span>Edit Multiple Student</span>
+						<svg
+							style={{ transform: value ? "rotate(180deg)" : "" }}
+							data-accordion-icon
+							className="w-6 h-6 shrink-0 transform rotate-180"
+							fill="currentColor"
+							viewBox="0 0 20 20"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+								clip-rule="evenodd"
+							></path>
+						</svg>
+					</button>
+				</h2>
+				<div
+					id="accordion-flush-body-3"
+					className={`${value ? "" : "hidden"}`}
+					aria-labelledby="accordion-flush-heading-3"
+				>
+					<div className="py-5 font-light">
+						<div className="flex flex-col gap-4">
+							<div className="row-span-full">
+								<label
+									htmlFor="score"
+									className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+								>
+									Change Exam where Score was:
+								</label>
+								<input
+									type="number"
+									name="score"
+									id="score"
+									value={String(examState.score)}
+									onChange={(e) =>
+										setExamState({
+											...examState,
+											error: "",
+											score: Number(e.target.value),
+										})
+									}
+									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+									placeholder="Old Score"
+									required
+								/>
+							</div>
+
+							<div className="row-span-full">
+								<label
+									htmlFor="date"
+									className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+								>
+									Change Exam where Date was:
+								</label>
+								<input
+									type="date"
+									name="date"
+									id="date"
+									value={examState?.date ? format(examState?.date, "yyyy-mm-dd") : ""}
+									onChange={({ target: { value } }) =>
+										setExamState({
+											...examState,
+											error: "",
+											date:
+												value && value.length > 1
+													? parse(value, "yyyy-mm-dd", new Date())
+													: null,
+										})
+									}
+									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+									placeholder="date"
+									required
+								/>
+							</div>
+							<div className="row-span-full">
+								<label
+									htmlFor="note"
+									className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+								>
+									change Exam where Note contains:
+								</label>
+								<input
+									type="text"
+									name="note"
+									id="note"
+									value={noteCondition}
+									onChange={(e) => setNoteCondition(e.target.value)}
+									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+									placeholder="Old Note"
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
 			<div className="row-span-full">
 				<label
 					htmlFor="score"
 					className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
 				>
-					Score
+					{value ? "New Score" : "Score"}
 				</label>
 				<input
 					type="number"
@@ -115,7 +230,7 @@ function AddExam({ onProceed, onClose, initialExam, profileIds, edit }: examInit
 						})
 					}
 					className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-					placeholder="Score"
+					placeholder={value ? "New Score" : "Score"}
 					required
 				/>
 			</div>
@@ -150,7 +265,7 @@ function AddExam({ onProceed, onClose, initialExam, profileIds, edit }: examInit
 					htmlFor="note"
 					className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
 				>
-					Note
+					{value ? "New Note" : "Note"}
 				</label>
 				<textarea
 					rows={4}
@@ -166,7 +281,7 @@ function AddExam({ onProceed, onClose, initialExam, profileIds, edit }: examInit
 						})
 					}
 					className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-					placeholder="Note"
+					placeholder={value ? "New Note" : "Note"}
 				/>
 			</div>
 			<button
