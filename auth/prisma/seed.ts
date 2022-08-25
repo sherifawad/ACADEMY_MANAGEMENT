16e6c9328eb524011aa141d152d39abcf213605c
Sherif Awad <eng.sherifawad@gmail.com>
2022-08-25 17:57:42 +0200

    prisma password model prisma seed

---
+++ auth/prisma/seed.ts
@@ -0,0 +1,53 @@
+import { PrismaClient, Role } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const client = new PrismaClient();
+
+async function seed() {
+	await client.userPassword.deleteMany();
+	await client.user.deleteMany();
+	await client.account.deleteMany();
+	await client.session.deleteMany();
+	await client.verificationToken.deleteMany();
+
+	const salt = await bcrypt.genSalt(
+		parseInt(process.env.SALT_ROUNDS || "10", 10)
+	);
+	const hashedPassword = await bcrypt.hash("adminPassword@root#1", salt);
+
+	const admin = await client.user.create({
+		data: {
+			name: "sherif awad",
+			role: Role.ADMIN,
+			email: "eng.sherifawad@gmail.com",
+			emailVerified: new Date(),
+			password: {
+				create: {
+					password: hashedPassword,
+					forceChange: false
+				}
+			}
+		},
+		include: {
+			password: true
+		}
+	});
+
+	const profile = await client.account.create({
+		data: {
+			userId: admin.id,
+			type: "credentials",
+			provider: "Credentials",
+			providerAccountId: "domain-login"
+		}
+	});
+}
+
+seed()
+	.catch(e => {
+		console.error(e);
+		process.exit(1);
+	})
+	.finally(async () => {
+		await client.$disconnect();
+	});
