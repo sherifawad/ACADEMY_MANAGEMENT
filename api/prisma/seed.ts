f013a2d426f4cedbb90008824e81a7764ff63375
Sherif Awad <eng.sherifawad@gmail.com>
2022-08-31 15:12:24 +0200

    add roles to api prisma

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -10,12 +10,88 @@ async function seed() {
 	await client.user.deleteMany();
 	await client.group.deleteMany();
 	await client.grade.deleteMany();
+	await client.domain.deleteMany();
+	await client.permission.deleteMany();
+	await client.role.deleteMany();
+
+	const Roles = await client.role.createMany({
+		data: [
+			{
+				name: "systemAdmin",
+			},
+			{
+				name: "admin",
+			},
+			{
+				name: "teacher",
+			},
+			{
+				name: "parent",
+			},
+			{
+				name: "student",
+			},
+			{
+				name: "guest",
+			},
+		],
+	});
+
+	const domains = await client.domain.createMany({
+		data: [
+			{
+				name: "user",
+			},
+			{
+				name: "student",
+			},
+			{
+				name: "grade",
+			},
+			{
+				name: "group",
+			},
+			{
+				name: "attendance",
+			},
+			{
+				name: "exam",
+			},
+			{
+				name: "assignment",
+			},
+		],
+	});
+
+	const permissions = await client.permission.createMany({
+		data: [
+			{
+				name: "full",
+			},
+			{
+				name: "delete",
+			},
+			{
+				name: "edit",
+			},
+			{
+				name: "deActivate",
+			},
+			{
+				name: "read",
+			},
+		],
+	});
 
 	const admin = await client.user.create({
 		data: {
 			id: "cl7au537v001443vyn6n56fbm",
-			name: "sherif awad",
-			role: Role.ADMIN,
+			name: "Sherif Awad",
+			roles: {
+				connect: {
+					name: "systemAdmin",
+				},
+			},
 		},
 	});
 }
